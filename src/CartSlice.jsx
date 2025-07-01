f29b6c1bb2de56ab814fd3b4414ae5dfc9f97b47
Carlos Araujo <carlos.araujo.aus@gmail.com>
2025-07-01 09:32:36 +0800

    State management using redux - CartSlice with reducers updated

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -8,13 +8,26 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const {name, image, cost} = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({name, image, cost, quantity: 1});
+      }
     },
     removeItem: (state, action) => {
+      const {name} = action.payload;
+      state.items = state.items.filter(item => item.name !== name);
     },
     updateQuantity: (state, action) => {
-
-    
+      const {name, quantity} = action.payload;
+      const itemToUpdate  = state.items.find(item => item.name === name);
+      if (itemToUpdate && quantity > 0) {
+        itemToUpdate.quantity = quantity;
+      } else if (quantity === 0) {
+        state.items = state.items.filter(item => item.name !== name);
+      }
     },
   },
 });
@@ -22,3 +35,4 @@ export const CartSlice = createSlice({
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
 
 export default CartSlice.reducer;
+
